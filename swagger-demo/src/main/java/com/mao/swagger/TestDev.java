a6e6c318b873d59ff5e2ce62e6155dbfe4ef853f
greatmanmxl <2439267299@qq.com>
2019-09-05 16:13:52 +0800

    dev修改2 3

--- swagger-demo/src/main/java/com/mao/swagger/TestDev.java
+++ swagger-demo/src/main/java/com/mao/swagger/TestDev.java
@@ -1,5 +1,9 @@
 package com.mao.swagger;
 
 public class TestDev {
-
+  public static void main(String[] args) {
+	System.out.println("dev");
+	System.out.println("dev2");
+	System.out.println("dev3");
+}
 }
